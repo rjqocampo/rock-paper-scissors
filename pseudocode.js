5486e3f116f326d03c21f98eec630e7308bd3d56
RJ Ocampo <rjqocampo@gmail.com>
2023-07-12 08:33:00 +0800

    Make pseudocode file and add multiple functions
    Make a function to parse the input of the user.
    
    Make a function to generate computer choice.
    
    Make a function to compare both inputs.

---
+++ pseudocode.js
@@ -0,0 +1,36 @@
+/**
+ * Get input from user via prompt method and store in a variable
+ * Store computer choice in a variable using a function that generates random number 1-3
+ * 
+ * Make a function to play one round
+ *  if user input and computer is same
+ *      inform draw
+ *      no score added
+ *  if user input is Rock
+ *      if computer input is Papers
+ *          then inform lost
+ *          add score to computer
+ *      if computer input is Scissors
+ *          then inform win
+ *          add score to user
+ * if user input is Papers
+ *      if computer input is Rock
+ *          then inform win
+ *          add score to user
+ *      if computer input is Scissors
+ *          then inform lost
+ *          add score to computer
+ * if user input is Scissors
+ *      if computer input Rock
+ *          then inform lost
+ *          add score to computer
+ *      if computer input Papers
+ *          then inform win
+ *          add score to user
+ * 
+ * Make a for loop to play rounds
+ *      if score is equal to 5
+ *          end the game and announce winner
+ *      else 
+ *          play another round
+ */
--- script.js
+++ script.js
@@ -1,1 +1,88 @@
-console.log("Hello World!")
+let input = prompt("Rock, Papers, Scissors?!")
+let computerSelection = getComputerChoice()
+let playerSelection = parseInput(input)
+let userScore;
+let computerScore;
+
+// function to check if input is empty or invalid. if correct input, make it case insensitive
+function parseInput () {
+    let letter = input.charAt(0)
+    let word = letter.toUpperCase() + input.slice(1).toLowerCase();
+
+    if (word === "") {
+        alert("Kindly choose between Rocks, Papers and Scissors.")
+    } else if (word === "Rock" || word === "Papers" || word === "Scissors") {
+        return word;
+    } else {
+        alert("You provided an invalid input.")
+    }
+}
+
+// function for a random number generator then assigns respectively if 'Rock', 'Paper', or 'Scissors'
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 3) + 1
+
+    if (n === 1) {
+        return "Rock";
+    } else if (n === 2) {
+        return "Papers";
+    } else {
+        return "Scissors";
+    }
+
+    // n === 1 ? "Rock"
+    // : n === 2 ? "Papers"
+    // : "Scissors";
+}
+
+// function to compare both inputs and add a score to the winner
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection + " VS " + computerSelection)
+    if (playerSelection === computerSelection) { 
+        console.log("Draw!")
+    }
+
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Papers") {
+            console.log("You lose! Opponent gets 1 point.")
+            return computerScore++;
+            
+        } else if (computerSelection === "Scissors") {
+            console.log("You win! You get 1 point.")
+            return userScore++;
+        }
+    }
+
+    if (playerSelection === "Papers") {
+        if (computerSelection === "Rock") {
+            console.log("You win! You get 1 point.")
+            return userScore++;
+        } else if (computerSelection === "Scissors") {
+            console.log("You lose! Opponent gets 1 point.")
+            return computerScore++;
+        }
+    }
+
+    if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            console.log("You lose! Opponent gets 1 point.") 
+            return computerScore++;
+        } else if (computerSelection === "Papers") {
+            console.log("You win! You get 1 point.")
+            return userScore++;
+        }
+    }
+}
+
+function game() {
+    userScore = 0;
+    computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Round ${i}. SCORE: ${userScore}:${computerScore}`)
+
+        playRound(playerSelection, computerSelection)
+    }
+}
+
+game();
