30c481ee3aec3276953c604810caed193c5eca6b
RJ Ocampo <rjqocampo@gmail.com>
2023-07-12 10:50:57 +0800

    Make a function to loop 5 rounds and determine winner

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-let input = prompt("Rock, Papers, Scissors?!")
-let computerSelection = getComputerChoice()
-let playerSelection = parseInput(input)
-let userScore;
-let computerScore;
-
 // function to check if input is empty or invalid. if correct input, make it case insensitive
-function parseInput () {
+function parseInput (input) {
     let letter = input.charAt(0)
     let word = letter.toUpperCase() + input.slice(1).toLowerCase();
 
     if (word === "") {
         alert("Kindly choose between Rocks, Papers and Scissors.")
-    } else if (word === "Rock" || word === "Papers" || word === "Scissors") {
+    } else if (word === "Rocks" || word === "Papers" || word === "Scissors") {
         return word;
     } else {
         alert("You provided an invalid input.")
@@ -23,7 +17,7 @@ function getComputerChoice() {
     let n = Math.floor(Math.random() * 3) + 1
 
     if (n === 1) {
-        return "Rock";
+        return "Rocks";
     } else if (n === 2) {
         return "Papers";
     } else {
@@ -35,53 +29,77 @@ function getComputerChoice() {
     // : "Scissors";
 }
 
-// function to compare both inputs and add a score to the winner
+// function to compare both choices
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection + " VS " + computerSelection)
     if (playerSelection === computerSelection) { 
         console.log("Draw!")
+        return "draw"
     }
 
-    if (playerSelection === "Rock") {
+    if (playerSelection === "Rocks") {
         if (computerSelection === "Papers") {
             console.log("You lose! Opponent gets 1 point.")
-            return computerScore++;
-            
+            return "lose"
         } else if (computerSelection === "Scissors") {
             console.log("You win! You get 1 point.")
-            return userScore++;
+            return "win"
         }
     }
 
     if (playerSelection === "Papers") {
-        if (computerSelection === "Rock") {
+        if (computerSelection === "Rocks") {
             console.log("You win! You get 1 point.")
-            return userScore++;
+            return "win"
         } else if (computerSelection === "Scissors") {
             console.log("You lose! Opponent gets 1 point.")
-            return computerScore++;
+            return "lose"
         }
     }
 
     if (playerSelection === "Scissors") {
-        if (computerSelection === "Rock") {
+        if (computerSelection === "Rocks") {
             console.log("You lose! Opponent gets 1 point.") 
-            return computerScore++;
+            return "lose"
         } else if (computerSelection === "Papers") {
             console.log("You win! You get 1 point.")
-            return userScore++;
+            return "win"
         }
     }
 }
 
+// function to run games 5 times and determine who wins after
 function game() {
-    userScore = 0;
-    computerScore = 0;
+    let userScore = 0;
+    let computerScore = 0;
 
     for (let i = 1; i <= 5; i++) {
-        console.log(`Round ${i}. SCORE: ${userScore}:${computerScore}`)
+        let input = prompt("Rocks, Papers, Scissors?!")
+        let playerSelection = parseInput(input)
+        let computerSelection = getComputerChoice()
+        console.log(playerSelection + " VS " + computerSelection)
 
-        playRound(playerSelection, computerSelection)
+        if (playRound(playerSelection, computerSelection) === "win") {
+            userScore++;
+            console.log(`Round ${i}. SCORE: ${userScore}:${computerScore}`)
+        } else if (playRound(playerSelection, computerSelection) === "lose") {
+            computerScore++
+            console.log(`Round ${i}. SCORE: ${userScore}:${computerScore}`)
+        } else {
+            console.log(`Round ${i}. SCORE: ${userScore}:${computerScore}`)
+        }
+
+        if (i === 5) {
+            if (userScore > computerScore) {
+                alert("YOU'VE WON THE GAME!")
+                console.log("YOU'VE WON THE GAME!")
+            } else if (userScore === computerScore) {
+                alert("THE GAME IS A DRAW!")
+                console.log("THE GAME IS A DRAW!")
+            } else {
+                alert("YOU'VE LOST THE GAME!")
+                console.log("YOU'VE LOST THE GAME!")
+            }
+        }
     }
 }
 
